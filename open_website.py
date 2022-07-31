1ba10f22ee82c0c6fc1a00f07d3bec5c68314c58
jaccopvh <88671147+jaccopvh@users.noreply.github.com>
2022-07-31 08:00:01 +0200

    working test case

--- open_website.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-driver = webdriver.Chrome(executable_path=r'C:\Users\jacco\Google Drive\QC\chromedriver_win32\chromedriver.exe')
-driver.maximize_window()
-driver.get('https://www.porvainn.eu')
-driver.find_element_by_link_text("prices and bookings").click()
